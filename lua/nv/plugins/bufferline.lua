f2e5176503a04e857e7ae2c602090b2e6ad1e731
rv-clalley <clalley@redventures.com>
2025-10-07 13:18:07 -0400

    feat: remove unused plugins

--- lua/nv/plugins/bufferline.lua
+++
@@ -1,9 +0,0 @@
-return {
-	'akinsho/bufferline.nvim',
-	event = "BufRead",
-	version = "*", 
-	dependencies = 'nvim-tree/nvim-web-devicons',
-	config = function ()
-		require("bufferline").setup{}
-	end
-}
