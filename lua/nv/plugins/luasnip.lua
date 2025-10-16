f991941d9afa8f419acfdf65b51abde7bd6c90ff
rv-clalley <clalley@redventures.com>
2025-10-16 11:30:50 -0400

    feat: add new snippet

--- lua/nv/plugins/luasnip.lua
+++ lua/nv/plugins/luasnip.lua
@@ -1,28 +1,38 @@
 return {
-  'L3MON4D3/LuaSnip',
-  version = "v2.*",
-  build = "make install_jsregexp",
-  dependencies = {
-    'rafamadriz/friendly-snippets',
-  },
-  config = function()
-    local ls = require('luasnip')
-    local s = ls.snippet
-    local t = ls.text_node
-    local i = ls.insert_node
-    
-    -- Load snippets from friendly-snippets
-    require('luasnip.loaders.from_vscode').lazy_load()
-    
-    -- Custom Go error handling snippet
-    ls.add_snippets("go", {
-      s("iferr", {
-        t("if err != nil {"),
-        t({"", "\treturn "}),
-        i(1),
-        t({"", "}"}),
-        i(0)
-      })
-    })
-  end,
+	'L3MON4D3/LuaSnip',
+	version = "v2.*",
+	build = "make install_jsregexp",
+	dependencies = {
+		'rafamadriz/friendly-snippets',
+	},
+	config = function()
+		local ls = require('luasnip')
+		local s = ls.snippet
+		local t = ls.text_node
+		local i = ls.insert_node
+
+		-- Load snippets from friendly-snippets
+		require('luasnip.loaders.from_vscode').lazy_load()
+		-- Custom Go error handling snippet
+
+		ls.add_snippets("go", {
+			s("iferr", {
+				t("if err != nil {"),
+				t({ "", "\treturn " }),
+				i(1),
+				t({ "", "}" }),
+				i(0)
+			})
+		})
+
+		ls.add_snippets("go", {
+			s("fmterr", {
+				t("if err != nil {"),
+				t({ "", "\treturn fmt.Errorf(\"" }),
+				i(1, ""), -- cursor will jump here
+				t({ " :%w\", err)", "", "}" }),
+			}),
+		})
+	end,
 }
+
